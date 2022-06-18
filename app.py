927c5a9fbce978acc046814fb161d2de4bd7dbc8
mcheng <90663512+Ekiiiim@users.noreply.github.com>
2022-06-18 17:51:12 -0500

    Add feature
    Capable of visiting a specific brain teaser.

--- app.py
+++ app.py
@@ -7,9 +7,23 @@ app = Flask(__name__)
 
 # flask run -> server runs at http://127.0.0.1:24000/ 
 @app.route("/")
-def get_riddle():
+def get_random_riddle():
     # generate the number of brain teaser
     num = random.randint(1, 999)
+    # extract the teaser from a riddles website
+    page = requests.get(f"https://www.riddles.com/{num}")
+    soup = BeautifulSoup(page.text, "lxml")
+    strs = soup.find_all("strong")
+    str1 = str(strs[0].parent)
+    str2 = str(strs[1].parent)
+    str1 = str1[str1.find('</strong>')+len('</strong>'):str1.rfind('</div>')]
+    str2 = str2[str2.find('</strong>:')+len('</strong>:'):str2.rfind('</div>')]
+    # return a web page
+    return render_template('template.html', number=num, question=str1, answer=str2)
+
+@app.route("/<num>")
+def get_a_riddle(num):
+    num = int(num)
     # extract the teaser from a riddles website
     page = requests.get(f"https://www.riddles.com/{num}")
     soup = BeautifulSoup(page.text, "lxml")
